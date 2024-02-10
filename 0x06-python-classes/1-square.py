8f88a49174149c7ed9bfca5929394a7f7351a2b8
probrahim <ibrahimhassani674@gmail.com>
2024-02-10 16:04:41 +0100

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Defines'''
+
+
+class Square:
+    ''' Represent'''
+    def __init__(self, size):
+        '''Initialize a new
+        Args:
+           size (int): The size.
+        '''
+        self.__size = size
