c5e504c27ca954d64eefb9fca13543ead38f1d68
probrahim <ibrahimhassani674@gmail.com>
2024-04-29 16:19:36 +0200

    task 00

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-
+"""print all states from hbtn0e0usa database"""
 import MySQLdb
-
-from sys import argv
-
+import sys
 
 
-if __name__ == '__main':
+def tuto():
+    args = sys.argv
+    mydb = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[1],
+        password=args[2],
+        database=args[3],
+        charset="utf8"
+    )
 
-    data = MySQLdb.connect(host="localhost" , port=3306, user=argv[1],passwd=argv[2], data=argv[3])
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    tuto = data.cursor()
-    tuto.execute("SELECT * FROM states")
+    result = mycursor.fetchall()
+    for row in result:
+        print(row)
 
-    row = tuto.fetchall()
-    for p in row:
-        print(i)
-    tuto.close()
-    data.close()
+
+if __name__ == "__main__":
+    tuto()
