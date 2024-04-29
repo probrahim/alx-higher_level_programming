b703136cdabb50abfa728af0557d5bde82552fd2
probrahim <ibrahimhassani674@gmail.com>
2024-04-30 00:06:19 +0200

    task 09

---
+++ 0x0F-python-object_relational_mapping/__pycache__/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+def plutot():
+
+    #debut de creation
+
+    moteur = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3], pool_pre_ping=True)
+
+    Session = sessionmaker(bind=moteur)
+
+    session = Session()
+    Base.metadta.create_all(moteur)
+
+    kumul = session.query(State).filter(State.name.like('%a%'))\
+            .order_by(State.id).all()
+    for i in kumul:
+        print("{}: {}".format(state.id, state.name)
+    session.close()
+
+    
+if __name__ == "__main__":
+    plutot()
