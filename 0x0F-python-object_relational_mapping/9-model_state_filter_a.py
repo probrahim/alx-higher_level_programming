5b6242a46bd907a02bf8bcf19c52295ff5d32dec
probrahim <ibrahimhassani674@gmail.com>
2024-04-30 00:31:05 +0200

    fix problme

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Show states from database """
 
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -6,26 +7,27 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 
-def plutot():
-
-    """debut de moteur creation"""
-
-    moteur = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+def show_states():
+    """ Main function to show states """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-
-    Session = sessionmaker(bind=moteur)
-
+    Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(moteur)
+    Base.metadata.create_all(engine)
 
-    kumul = session.query(State).filter(State.name.like('%a%'))\
+    # Retrieve states containing 'a' and order by ID
+    states_with_a = session.query(State).filter(State.name.like('%a%'))\
         .order_by(State.id).all()
-    for i in kumul:
-        if "a" in i.name:
-            print("{}: {}".format(i.id, i.name))
+
+    if states_with_a:
+        for state in states_with_a:
+            print("{}: {}".format(state.id, state.name))
+    else:
+        print("No record")
+
     session.close()
 
 
 if __name__ == "__main__":
-    plutot()
+    show_states()
